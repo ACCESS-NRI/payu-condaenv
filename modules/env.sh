014c283670b52d691aedba1161ad6907096f7d83
Jo Basevi <johanna.basevi@anu.edu.au>
2024-04-10 13:44:10 +1000

    Update modules: check for pre-release modulefiles location

--- modules/env.sh
+++ modules/env.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Prints the environment variables when activating un-packed payu environment
+# Prints the environment variables when activating un-packed environment
 export PATH=/usr/bin:/bin
-source /g/data/vk83/apps/$1/bin/activate
+
+name=$1
+is_prerelease=$2
+
+if [ "$is_prerelease" = true ]; then
+    # Use prerelease location for apps
+    source /g/data/vk83/prerelease/apps/$name/bin/activate
+else
+    # Use release location for apps
+    source /g/data/vk83/apps/$name/bin/activate
+fi
 /bin/env
